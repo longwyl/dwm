cae805e6292de1041ba8edb09faa69afa8e33091
kofjoker <kofjoker@gmail.com>
2021-06-07 16:10:38 +0800

    mod+c spawn chromium mod+g spawn brave

--- config.def.h
+++ config.def.h
@@ -64,7 +64,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *broswer[]  = { "brave", NULL };
+static const char *broswer1[]  = { "brave", NULL };
+static const char *broswer2[]  = { "chromium", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
@@ -72,7 +73,8 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,            	        XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,               	XK_g,      spawn,          {.v = broswer } },
+	{ MODKEY,               	XK_g,      spawn,          {.v = broswer1 } },
+	{ MODKEY,               	XK_c,      spawn,          {.v = broswer2 } },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_b,      hideborder,     {0} },
